554e40323161ff758dd23c4e2352f460686f7804
vahin5111 <99596827+vahin5111@users.noreply.github.com>
2022-03-21 19:03:15 +0530

    Update GameController.java
    Comment added

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -16,6 +16,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  * Game of life, demonstration application for Jenkins: The Definitive Guide
  *
  * GameController.java
+ * New comment added for testing
  * Controller to navigate the buttons on the game pages
  */
 @Controller
